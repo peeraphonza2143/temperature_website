e2e6a11d28426c951f9fb8bf3cdf801b2f6c9104
SuphonSaejia <bahgza@gmail.com>
2021-10-04 15:21:32 +0700

    ดึงข้อมูลจาก api มาแสดงได้แล้ว

--- script/main_chart.js
+++ script/main_chart.js
@@ -1,39 +1,49 @@
-google.charts.load('current', {'packages':['corechart']});
-google.charts.setOnLoadCallback(drawChart);
-
-        function drawChart() {
-
-        $.ajax({
-          dataType: "json",
-          url: 'http://localhost:3000/localhost/mainchart',
-          success: function(response) {
-            console.log(response);
-          }
-        });
-
-        var data = google.visualization.arrayToDataTable([
-          ['Times', 'Sensor id', 'Sensor id','Sensor id'],
-          [ 12.00,   30,      40,      50],
-          [ 12.15,   40,      45,     30],
-          [ 12.30,   45,      48,     30],
-          [ 12.45,   30,      20,     10]
-        ]);
-        var options = {
-          fontName:'TCM',
-          hAxis: {
-            title: '%RH / Times',
-            titleTextStyle: {
-                color: "#707070",
-                fontName: "TCM",
-                fontSize: 18,
-                bold: false,
-                italic: false
+(function () {
+  `use strict`;
+fetch('http://a95c-2001-44c8-451b-e124-a00-27ff-feee-6352.ngrok.io/mainchart')
+  .then(response => response.json())
+  .then(data => getdata(data))
+  .catch(
+    error => console.log(error)
+  );
+function getdata(dataC){
+  google.charts.load('current', {'packages':['corechart']});
+  google.charts.setOnLoadCallback(drawChart);
+    function drawChart() {
+          var data =  new google.visualization.DataTable();
+          data.addColumn('string', 'Time');
+          data.addColumn('number', 'Sensor 1');
+          data.addColumn('number', 'Sensor 2');
+          var i = 0 , s;
+          for (v in dataC) {
+            i=i+1;
+            if(i%2 == 0){
+              var temp=[];
+              temp.push(dataC[v]['time']);
+              temp.push(dataC[v]['humidity_1']);
+              temp.push(s['humidity_1']);
+              data.addRow(temp);
             }
-          },
-          chartArea:{left:80,right:150,top:40,width:"100%",height:"80%"},
-          colors: ['#BED8D6', '#EE987F', '#F4D9AB'],
-        };
-        
-        var chart = new google.visualization.AreaChart(document.getElementById('chart_main'));
-        chart.draw(data, options);
-      }
+            s = dataC[v];
+          }
+          var options = {
+            fontName:'TCM',
+            hAxis: {
+              title: 'Day',
+              titleTextStyle: {
+                  color: "#707070",
+                  fontName: "TCM",
+                  fontSize: 18,
+                  bold: false,
+                  italic: false
+              }
+            },
+            chartArea:{left:80,right:150,top:40,width:"100%",height:"70%"},
+            colors: ['#BED8D6', '#EE987F', '#F4D9AB'],
+          };
+          
+          var chart = new google.visualization.AreaChart(document.getElementById('chart_main'));
+          chart.draw(data, options);
+        }
+}
+    })();    
--- script/sup_chart.js
+++ script/sup_chart.js
@@ -1,17 +1,36 @@
+(function () {
+`use strict`;
+fetch('http://a95c-2001-44c8-451b-e124-a00-27ff-feee-6352.ngrok.io/subchart1')
+  .then(response => response.json())
+  .then(data => getdata(data))
+  .catch(
+    error => console.log(error)
+  );
+  function getdata(dataC){   
 google.charts.load('current', {'packages':['corechart']});
 google.charts.setOnLoadCallback(drawChart);
       function drawChart() {
-        var data = google.visualization.arrayToDataTable([
-          ['Times', 'Sensor id', 'Sensor id','Sensor id'],
-          [ 12.00,   30,      40,      50],
-          [ 12.15,   40,      45,     30],
-          [ 12.30,   45,      48,     30],
-          [ 12.45,   30,      20,     10]
-        ]);
+        var data =  new google.visualization.DataTable();
+        data.addColumn('string', 'Time');
+        data.addColumn('number', 'Sensor 1');
+        data.addColumn('number', 'Sensor 2');
+        var i = 0 , s;
+        for (v in dataC) {
+          i=i+1;
+          if(i%2 == 0){
+            var temp=[];
+            temp.push(dataC[v]['week']);
+            temp.push(dataC[v]['humidity_2']);
+            temp.push(s['humidity_2']);
+            console.log(temp);
+            data.addRow(temp);
+          }
+          s = dataC[v];
+        }
         var options = {
           fontName:'TCM',
           hAxis: {
-            title: '%RH / Times',
+            title: 'Month',
             titleTextStyle: {
                 color: "#707070",
                 fontName: "TCM",
@@ -20,10 +39,12 @@ google.charts.setOnLoadCallback(drawChart);
                 italic: false
             }
           },
-          chartArea:{left:50,right:90,top:40,width:"70%",height:"60%"},
+          chartArea:{left:50,right:120,top:40,width:"70%",height:"60%"},
           colors: ['#BED8D6', '#EE987F', '#F4D9AB'],
         };
         
         var chart = new google.visualization.AreaChart(document.getElementById('chart_sup'));
         chart.draw(data, options);
       }
+    }
+    })();
