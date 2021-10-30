dbaeda1ec552bb5cecd06eb8935710f4edb369fd
ReemWSW <widsawa.d31@gmail.com>
2021-10-30 08:00:38 +0700

    [ADD] route query week

--- routes/humidity.route.js
+++ routes/humidity.route.js
@@ -1,26 +1,7 @@
-const express = require('express');
 const humidityModel = require('../models/humidity');
 
+const express = require('express');
 const humidityRoute = express.Router();
-
-humidityRoute.route('/api').get((req, res, next) => {
-  var today = new Date(); // Date right now
-  var date = today.getFullYear() + '-' + prependZero((today.getMonth() + 1)) + '-' + prependZero(today.getDate()); // 2021-10-10  
-
-  humidityModel.find({
-    time_stamp: {
-      $gt: new Date(`${date}T00:00:00.625z`),
-      $lte: new Date(`${date}T23:55:00.625z`)
-    }, sensor_id: reqSensor ? reqSensor : 1
-  }, (error, data) => {
-    if (error) {
-      return next(error)
-    } else {
-      console.log(data);
-      res.json(data)
-    }
-  })
-})
 
 humidityRoute.route('/api/realtime').get((req, res, next) => {
   var today = new Date();
@@ -47,17 +28,41 @@ humidityRoute.route('/api/week').get((req, res, next) => {
   var month = dateCon.getMonth() + 1
   var year = dateCon.getFullYear()
 
-  humidityModel.findOne({}, (error, data) => {
+  var weeek1 = []
+  var weeek2 = []
+  var weeek3 = []
+  var weeek4 = []
+  var weeek5 = []
+
+  humidityModel.findOne({}, (error, dataOne) => {
     humidityModel.find({
       timestamp: {
         $gte: new Date(`${year}-${month}-01T00:00:00.000`),
         $lte: new Date(`${year}-${month + 1}-01T00:00:00.000`)
-      }, sensor: data.sensor
+      }, sensor: dataOne.sensor
     }, (error, data) => {
       if (error) {
         next(error)
       } else {
-        res.json({ data });
+        for (let index = 0; index < data.length; index++) {
+          const element = data[index];
+
+          var datetime = new Date(element.timestamp);
+          var date = datetime.getDate()
+
+          if (date <= 7) {
+            weeek1.push(element)
+          } else if (date <= 14) {
+            weeek2.push(element)
+          } else if (date <= 21) {
+            weeek3.push(element)
+          } else if (date <= 28) {
+            weeek4.push(element)
+          } else {
+            weeek5.push(element)
+          }
+        }
+        res.json([weeek1, weeek2, weeek3, weeek4, weeek5]);
       }
     })
   })
