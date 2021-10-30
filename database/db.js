0a576b28170a29f16495e19e2738dc0c38270759
ReemWSW <widsawa.d31@gmail.com>
2021-10-30 14:49:31 +0700

    [ADD] database clound

--- database/db.js
+++ database/db.js
@@ -1,3 +1,4 @@
 module.exports = {
-    db: 'mongodb://localhost:27017/humidityDB'
+    // db: 'mongodb://localhost:27017/humidityDB'
+    db: 'mongodb+srv://admin:1234@cluster0.ibqod.mongodb.net/humidityDB?retryWrites=true&w=majority'
 };
