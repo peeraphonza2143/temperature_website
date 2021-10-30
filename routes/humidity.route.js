f4e29227b248d5d63d8020af992dad55fb9f5a00
ReemWSW <widsawa.d31@gmail.com>
2021-10-30 09:01:55 +0700

    [EDIT] find everange weekly humidity value

--- routes/humidity.route.js
+++ routes/humidity.route.js
@@ -28,11 +28,11 @@ humidityRoute.route('/api/week').get((req, res, next) => {
   var month = dateCon.getMonth() + 1
   var year = dateCon.getFullYear()
 
-  var weeek1 = []
-  var weeek2 = []
-  var weeek3 = []
-  var weeek4 = []
-  var weeek5 = []
+  var week1 = []
+  var week2 = []
+  var week3 = []
+  var week4 = []
+  var week5 = []
 
   humidityModel.findOne({}, (error, dataOne) => {
     humidityModel.find({
@@ -51,25 +51,78 @@ humidityRoute.route('/api/week').get((req, res, next) => {
           var date = datetime.getDate()
 
           if (date <= 7) {
-            weeek1.push(element)
+            week1.push(element.humidity)
           } else if (date <= 14) {
-            weeek2.push(element)
+            week2.push(element.humidity)
           } else if (date <= 21) {
-            weeek3.push(element)
+            week3.push(element.humidity)
           } else if (date <= 28) {
-            weeek4.push(element)
+            week4.push(element.humidity)
           } else {
-            weeek5.push(element)
+            week5.push(element.humidity)
           }
         }
-        res.json([weeek1, weeek2, weeek3, weeek4, weeek5]);
+
+        var sumWeek1 = 0;
+        for (var i = 0; i < week1.length; i++) {
+          sumWeek1 += week1[i]
+        }
+        var avgWeek1 = sumWeek1 / week1.length;
+
+        var sumWeek2 = 0;
+        for (var i = 0; i < week2.length; i++) {
+          sumWeek2 += week2[i]
+        }
+        var avgWeek2 = sumWeek2 / week2.length;
+
+        var sumWeek3 = 0;
+        for (var i = 0; i < week3.length; i++) {
+          sumWeek3 += week3[i]
+        }
+        var avgWeek3 = sumWeek3 / week3.length;
+
+        var sumWeek4 = 0;
+        for (var i = 0; i < week4.length; i++) {
+          sumWeek4 += week4[i]
+        }
+        var avgWeek4 = sumWeek4 / week4.length;
+
+        var sumWeek5 = 0;
+        for (var i = 0; i < week5.length; i++) {
+          sumWeek5 += week5[i]
+        }
+        var avgWeek5 = sumWeek5 / week5.length;
+
+        res.json([avgWeek1, avgWeek2, avgWeek3, avgWeek4, avgWeek5]);
       }
     })
   })
 })
 
 humidityRoute.route('/api/year').get((req, res, next) => {
-  humidityModel.find({})
+
+
+  var dateCon = new Date()
+  var year = dateCon.getFullYear()
+  humidityModel.findOne((error, dataOne) => {
+    humidityModel.find({
+      timestamp: {
+        $gte: new Date(`${year}-01-01T00:00:00.000`),
+        $lte: new Date(`${year}-12-31T23:00:00.000`)
+      }, sensor: dataOne.sensor
+    }, (error, data) => {
+      if (error) {
+        next(error)
+      } else {
+        for (let index = 0; index < data.length; index++) {
+          const element = data[index];
+
+          console.log(element);
+        }
+        res.json(data);
+      }
+    })
+  })
 })
 
 // number return 2 digit
