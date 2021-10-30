f288fba9f116574ad3718f4c82f0bc9441271c62
peeraphonza2143 <peeraphonza2143@gmail.com>
2021-10-30 08:41:48 +0000

    edit Time in grap 15 min

--- public/script/main_chart.js
+++ public/script/main_chart.js
@@ -18,18 +18,21 @@ var ip = '192.168.2.49:4000';
       data.addColumn('number', 'Hr');
       
       for (v in dataC) {
-        function convertFromStringToDate(responseDate) {
-          let dateComponents = responseDate.split('T');
-          let datePieces = dateComponents[0].split("-");
-          let timePieces = dateComponents[1].split(":");
-          return (new Date(datePieces[2], (datePieces[1] - 1), datePieces[0],
-            timePieces[0], timePieces[1], timePieces[2]))
+        function convertFromStringToDate(Time_a) {
+          var Time = Time_a.toString();
+          var Time_split1 = Time.split("T");
+          var Time_split2 = Time_split1[1].split(".");
+          var Time_split3 = Time_split2[0].split(":");
+          var Time_split4 = Time_split3[0] + ":" + Time_split3[1] ;
+          
+          return Time_split4
         }
         // 2012-01-01T00:00:00.000+00:00
         // var date = convertFromStringToDate('21-03-2020T11:20:30')
-        console.log(dataC[v]['timestamp'][0]);
+       // console.log(dataC[v]['timestamp']);
+       convertFromStringToDate(dataC[v]['timestamp']);
         temp = [];
-        temp.push(dataC[v]['timestamp']);
+        temp.push(convertFromStringToDate(dataC[v]['timestamp']));
         temp.push(dataC[v]['humidity']);
         data.addRow(temp);
 
